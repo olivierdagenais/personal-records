83f3245bc18bc370afccae360d3236cedd4e6839
Olivier Dagenais <olivier.dagenais@gmail.com>
2023-09-03 21:52:15 -0400

    feat: simple list of tables, w/fake data

---
+++ PersonalRecords/Models/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonalRecords.Models;
+
+public record Table
+{
+	public string Name { get; init; }
+
+	public string Type { get; init; }
+
+	public string DisplayName { get; init; }
+
+	public string Description { get; init; }
+
+	public DateTime LastEntryDate { get; init; }
+
+	public string LastEntryDescription { get; init; }
+
+	public string LastEntryValue { get; init; }
+}
